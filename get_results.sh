4c43ce7e400d98a679487b351b4a1d8e5f182ff1
David Jr Akre <dakre@Davids-Mac-mini.local>
2017-11-20 23:40:39 -0700

    Adding hdfs merge functionality + tweak to merge

--- get_results.sh
+++ get_results.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+hadoop fs -getmerge /user/`whoami`/output/ output/final.txt
 hadoop fs -get /user/`whoami`/output
+
--- merge.py
+++ merge.py
@@ -1,23 +1,5 @@
-with open('output/part-r-00000') as f:
-    read_data_0 = f.read().split()
-
-with open('output/part-r-00001') as f:
-    read_data_1 = f.read().split()
-
-with open('output/part-r-00002') as f:
-    read_data_2 = f.read().split()
-
-with open('output/part-r-00003') as f:  
-    read_data_3 = f.read().split()
-
-with open('output/part-r-00004') as f:
-    read_data_4 = f.read().split()
-
-with open('output/part-r-00005') as f: 
-    read_data_5 = f.read().split()
-
-with open('output/part-r-00006') as f:
-    read_data_6 = f.read().split()
+with open('output/final.txt') as f:
+    read_data = f.read().split()
 
 max_1 = 0
 max_2 = 0
@@ -30,7 +12,7 @@ max_web_3 = ""
 max_web_4 = ""
 max_web_5 = ""
 
-for data in read_data_0:
+for data in read_data:
     if "website" in data:
         hold = data
     else:
@@ -69,169 +51,6 @@ for data in read_data_0:
         elif data > max_5:
             max_5 = data
             max_web_5 = hold
-
-for data in read_data_1:
-    if "website" in data:
-        hold = data
-    else:
-        if data > max_1:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = max_2
-            max_web_3 = max_web_2
-            max_2 = max_1
-            max_web_2 = max_web_1
-            max_1 = data
-            max_web_1 = hold
-        elif data > max_2:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = max_2
-            max_web_3 = max_web_2
-            max_2 = data
-            max_web_2 = hold
-        elif data > max_3:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = data
-            max_web_3 = hold
-        elif data > max_4:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = data
-            max_web_4 = hold
-        elif data > max_5:
-            max_5 = data
-            max_web_5 = hold
-
-for data in read_data_2:
-    if "website" in data:
-        hold = data
-    else:
-        if data > max_1:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = max_2
-            max_web_3 = max_web_2
-            max_2 = max_1
-            max_web_2 = max_web_1
-            max_1 = data
-            max_web_1 = hold
-        elif data > max_2:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = max_2
-            max_web_3 = max_web_2
-            max_2 = data
-            max_web_2 = hold
-        elif data > max_3:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = data
-            max_web_3 = hold
-        elif data > max_4:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = data
-            max_web_4 = hold
-        elif data > max_5:
-            max_5 = data
-            max_web_5 = hold
-
-
-for data in read_data_3:
-    if "website" in data:
-        hold = data
-    else:
-        if data > max_1:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = max_2
-            max_web_3 = max_web_2
-            max_2 = max_1
-            max_web_2 = max_web_1
-            max_1 = data
-            max_web_1 = hold
-        elif data > max_2:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = max_2
-            max_web_3 = max_web_2
-            max_2 = data
-            max_web_2 = hold
-        elif data > max_3:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = data
-            max_web_3 = hold
-        elif data > max_4:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = data
-            max_web_4 = hold
-        elif data > max_5:
-            max_5 = data
-            max_web_5 = hold
-
-for data in read_data_4:
-    if "website" in data:
-        hold = data
-    else:
-        if data > max_1:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = max_2
-            max_web_3 = max_web_2
-            max_2 = max_1
-            max_web_2 = max_web_1
-            max_1 = data
-            max_web_1 = hold
-        elif data > max_2:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = max_2
-            max_web_3 = max_web_2
-            max_2 = data
-            max_web_2 = hold
-        elif data > max_3:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = max_3
-            max_web_4 = max_web_3
-            max_3 = data
-            max_web_3 = hold
-        elif data > max_4:
-            max_5 = max_4
-            max_web_5 = max_web_4
-            max_4 = data
-            max_web_4 = hold
-        elif data > max_5:
-            max_5 = data
-            max_web_5 = hold
-
-
 
 
 print "Most frequently viewed web sites (top 5):"           
